f708c3070ff9fc604fc2190829ab0add17bd457b
apple <melvinfernando175@gmail.com>
2021-04-03 22:32:32 +0530

    removing unnecessary component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Header from './Header'
 import Body from './Body'
-import Footer from './Footer'
+
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <Body/>
-     {/* <Footer/> */}
     </div>
   );
 }
